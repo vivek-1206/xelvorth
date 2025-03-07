104ad0cf24691b0d4e394f19d61c9a4e328e67ae
vivek-1206 <vivek.08vb@gmail.com>
2025-03-07 17:59:02 +0530

    sample

---
+++ lib/Features/kyc/kyc.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Kyc extends StatefulWidget {
+  const Kyc({ Key? key }) : super(key: key);
+
+  @override
+  _KycState createState() => _KycState();
+}
+
+class _KycState extends State<Kyc> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Features/portfolio/portfolio.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Portfolio extends StatefulWidget {
+  const Portfolio({ Key? key }) : super(key: key);
+
+  @override
+  _PortfolioState createState() => _PortfolioState();
+}
+
+class _PortfolioState extends State<Portfolio> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
